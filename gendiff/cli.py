2baf597c5d12c8fa455f9e445d931914a4d355c4
borisovaldv <borisovaldv@gmail.com>
2022-12-01 14:34:26 +0300

    Update gendiff script

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -1,17 +1,10 @@
-#!/usr/bin/env python3
-
-
 import argparse
 
 
-def get_args():
+def parse_args():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser._optionals.title = "optional arguments"
     args = parser.parse_args()
     return args
-
-
-if __name__ == '__main__':
-    get_args()
---
+++ gendiff/scripts/run_gendiff.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+from gendiff import cli
+import argparse
+
+
+def main():
+    args = cli.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    main()
